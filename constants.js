bb22cb9c427303241dfb91ab1ced8a9a7f1a8247
David Inglody <inglody96@gmail.com>
2024-02-21 16:12:33 +0100

    first commit

---
+++ constants.js
@@ -0,0 +1,10 @@
+export const transactionViewOptions = ["Yearly", "Monthly", "Daily"];
+export const categories = [
+  "Food",
+  "Transport",
+  "Entertainment",
+  "Health",
+  "Bills",
+  "Others",
+];
+export const types = ["Income", "Expense", "Saving", "Investment"];
